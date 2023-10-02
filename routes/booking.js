2b97df9ffb68365f2027d8ba7a643ef88b1732ae
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2023-10-02 11:56:31 +0700

    first commit

---
+++ routes/booking.js
@@ -0,0 +1,18 @@
+'use strict'
+const express = require("express")
+const auth= require("../auth/auth")
+
+const bookingController = require("../controllers/booking")
+const router = new express.Router()
+
+router.post("/add", auth.authVerify, bookingController.addBookingRoom)
+router.put("/update/status/:id_booking", auth.authVerify, bookingController.updateStatusBooking)
+router.delete("/delete/:id_booking", auth.authVerify, bookingController.deleteOneBooking)
+
+router.get("/", bookingController.getAllBooking)
+router.get("/:id_booking", bookingController.getOneBooking)
+router.post("/find/name-customer", bookingController.findBookingByNameCustomer)
+router.post("/find/filter", bookingController.findBookingDataFilter)
+router.get("/customer/:id_customer", bookingController.findBookingByIdCustomer)
+
+module.exports = router
